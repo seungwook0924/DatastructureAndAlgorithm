017bca0e99105b1f280e635a5e81db1046427b74
Lee SeungWook <leesw1998@gmail.com>
2024-07-04 15:18:31 +0900

    24.07.4

---
+++ src/inflearn_algorithm/sec01_03.java
@@ -0,0 +1,29 @@
+package inflearn_algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class sec01_03 {
+    public static String solution(String str) {
+        StringTokenizer st = new StringTokenizer(str);
+        int max = 0;
+        String text = "";
+        while (st.hasMoreTokens()){
+            String temp = st.nextToken();
+            int length = temp.length();
+            if(length > max) {
+                max = length;
+                text = temp;
+            }
+        }
+        return text;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text = br.readLine();
+        System.out.println(solution(text));
+    }
+}
