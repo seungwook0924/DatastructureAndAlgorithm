4d8d4fdac2914188b67d91a7e242de16cbcf7849
Lee SeungWook <leesw1998@gmail.com>
2024-07-08 15:36:44 +0900

    24.07.08

---
+++ src/inflearn_algorithm/sec01_09.java
@@ -0,0 +1,24 @@
+package inflearn_algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sec01_09 {
+    public static String solution(String str) {
+        String result = "";
+        for (char c : str.toCharArray())
+        {
+            if(Character.isDigit(c))
+            {
+                if(!(c == '0') || !(result.length() == 0)) result += c;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(br.readLine()));
+    }
+}
