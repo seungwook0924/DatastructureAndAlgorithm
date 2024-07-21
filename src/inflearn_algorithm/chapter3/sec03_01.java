bb71f1a4af0f26614024ea4b494b3dddf8250c93
SeungWook_Lee <leesw_0924@icloud.com>
2024-07-21 21:02:04 +0900

    24.07.21

---
+++ src/inflearn_algorithm/chapter3/sec03_01.java
@@ -0,0 +1,13 @@
+package inflearn_algorithm.chapter3;
+
+import java.io.IOException;
+
+public class sec03_01 {
+    public static void solution() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
