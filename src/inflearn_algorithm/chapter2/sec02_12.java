5dd19cbdc928ce306d14f8a8aebcf481829615ac
SeungWook_Lee <leesw_0924@icloud.com>
2024-07-22 14:08:47 +0900

    24.07.22

--- src/inflearn_algorithm/chapter2/sec02_12.java
+++ src/inflearn_algorithm/chapter2/sec02_12.java
@@ -6,29 +6,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class sec02_12 {
-    public static int solution(int n, int m, int[][] arr){
-        int answer=0;
-        for(int i = 1; i <= n; ++i)
-        {
-            for(int j = 1;  j<= n; ++j)
-            {
-                int cnt = 0;
-                for(int k = 0; k < m; ++k)
-                {
-                    int pi = 0, pj = 0;
-                    for(int s = 0; s < n; ++s)
-                    {
-                        if(arr[k][s] == i) pi = s;
-                        if(arr[k][s] == j) pj = s;
-                    }
-                    if(pi < pj) cnt++;
+    public static int solution(int N, int M, int[][] arr){
+        int count = 0;
+
+        // 순위를 저장할 배열
+        int[][] rank = new int[M][N + 1];
+
+        // 각 테스트에서 학생들의 순위를 미리 계산
+        for (int k = 0; k < M; ++k) {
+            for (int s = 0; s < N; ++s) {
+                rank[k][arr[k][s]] = s;
+                System.out.print(rank[k][s] + " ");
+            }
+            System.out.println();
+        }
+
+        // 학생 쌍 (i, j) 에 대해 비교
+        for (int i = 1; i <= N; ++i) {
+            for (int j = 1; j <= N; ++j) {
+                int temp = 0;
+                for (int k = 0; k < M; ++k) {
+                    if (rank[k][i] < rank[k][j]) ++temp;
                 }
-                if(cnt == m){
-                    answer++;
+                if (temp == M) {
+                    ++count;
+                    System.out.println(i + " " + j);
                 }
             }
         }
-        return answer;
+
+        return count;
     }
 
     public static void main(String[] args) throws IOException {
