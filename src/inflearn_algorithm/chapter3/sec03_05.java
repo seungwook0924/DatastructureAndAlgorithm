fab938c35574aca11218913c67cab265014b8e3e
SeungWook_Lee <leesw_0924@icloud.com>
2024-07-27 19:58:43 +0900

    24.07.27

--- src/inflearn_algorithm/chapter3/sec03_05.java
+++ src/inflearn_algorithm/chapter3/sec03_05.java
@@ -5,18 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class sec03_05 {
+//    public static int solution(int N) {
+//        int count = 0, sum = 0;
+//        int lPtr = 0, halfPlusOne = (N / 2) + 1;
+//        int[] arr = new int[halfPlusOne];
+//
+//        for(int i = 0; i < halfPlusOne; ++i) arr[i] = i + 1;
+//
+//        for(int rPtr = 0; rPtr < halfPlusOne; ++rPtr)
+//        {
+//            sum += arr[rPtr];
+//            while (sum > N) sum -= arr[lPtr++];
+//            if(sum == N) ++count;
+//        }
+//
+//        return count;
+//    }
+
     public static int solution(int N) {
-        int count = 0, sum = 0;
-        int lPtr = 0, halfPlusOne = (N / 2) + 1;
-        int[] arr = new int[halfPlusOne];
-
-        for(int i = 0; i < halfPlusOne; ++i) arr[i] = i + 1;
-
-        for(int rPtr = 0; rPtr < halfPlusOne; ++rPtr)
+        int count = 0, temp = 1;
+        --N;
+        while (N > 0)
         {
-            sum += arr[rPtr];
-            while (sum > N) sum -= arr[lPtr++];
-            if(sum == N) ++count;
+            N = N - (++temp);
+            if(N % temp == 0) ++count;
         }
 
         return count;
