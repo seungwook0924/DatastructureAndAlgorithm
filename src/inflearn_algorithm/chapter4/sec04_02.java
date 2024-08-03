53748d13b8d251ef7892f707d39eeedaab0995ba
SeungWook_Lee <leesw_0924@icloud.com>
2024-08-03 22:37:20 +0900

    24.08.03

--- src/inflearn_algorithm/chapter4/sec04_02.java
+++ src/inflearn_algorithm/chapter4/sec04_02.java
@@ -6,18 +6,19 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class sec04_02 {
-    public static String solution(String str1, String str2) {
+    public static String solution(String str1, String str2)
+    {
         if(str1.length() != str2.length()) return "NO";
         HashMap<Character, Integer> map1 = new HashMap();
         HashMap<Character, Integer> map2 = new HashMap();
-        for(int i = 0; i < str1.length(); ++i) {
+        
+        for(int i = 0; i < str1.length(); ++i)
+        {
             map1.put(str1.charAt(i), map1.getOrDefault(str1.charAt(i), 0) + 1);
             map2.put(str2.charAt(i), map2.getOrDefault(str2.charAt(i), 0) + 1);
         }
 
-        for(char c : map1.keySet()){
-            if(!(map2.containsKey(c) && (map2.get(c).equals(map1.get(c))))) return "NO";
-        }
+        for(char c : map1.keySet()) if(!(map2.containsKey(c) && (map2.get(c).equals(map1.get(c))))) return "NO";
 
         return "YES";
     }
