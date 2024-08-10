8b0c2613a733f514ae397bbc8df92bde7d5873d1
SeungWook_Lee <leesw_0924@icloud.com>
2024-08-10 13:21:19 +0900

    24.08.10

---
+++ src/inflearn_algorithm/chapter5/sec05_04.java
@@ -0,0 +1,30 @@
+package inflearn_algorithm.chapter5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class sec05_04 {
+    public static int solution(String str) {
+        Stack<Integer> stack = new Stack<>();
+        for (Character c : str.toCharArray()) {
+            if(Character.isDigit(c)) stack.push(Character.getNumericValue(c));
+            else
+            {
+                int right = stack.pop();
+                int left = stack.pop();
+                if(c == '+') stack.push(left + right);
+                else if(c == '-') stack.push(left - right);
+                else if(c == '*') stack.push(left * right);
+                else if(c == '/') stack.push(left / right);
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(br.readLine()));
+    }
+}
