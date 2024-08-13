1f3792f69ae496477e7805a12ad75cb4703d3485
SeungWook_Lee <leesw_0924@icloud.com>
2024-08-13 14:00:57 +0900

    24.08.13

--- src/inflearn_algorithm/chapter5/sec05_07.java
+++ src/inflearn_algorithm/chapter5/sec05_07.java
@@ -1,4 +1,22 @@
 package inflearn_algorithm.chapter5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class sec05_07 {
+    public static String solution(String sequence, String str) {
+        Queue<Character> que = new LinkedList<>();
+        for (char c : sequence.toCharArray()) que.add(c);
+        for (char c : str.toCharArray()) if(!que.isEmpty() && que.peek() == c) que.poll();
+        if(!que.isEmpty()) return "NO";
+        return "YES";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(br.readLine(), br.readLine()));
+    }
 }
