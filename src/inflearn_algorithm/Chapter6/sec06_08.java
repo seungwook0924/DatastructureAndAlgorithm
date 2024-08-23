6442f5cf1e1861bdbb4436fe04c5156b656d434a
SeungWook_Lee <leesw_0924@icloud.com>
2024-08-23 21:18:36 +0900

    24.08.23

--- src/inflearn_algorithm/Chapter6/sec06_08.java
+++ src/inflearn_algorithm/Chapter6/sec06_08.java
@@ -7,9 +7,25 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class sec06_08 {
+//    public static int solution(int[] arr, int M) {
+//        Arrays.sort(arr);
+//        return Arrays.binarySearch(arr, M) + 1;
+//    }
+
     public static int solution(int[] arr, int M) {
         Arrays.sort(arr);
-        return Arrays.binarySearch(arr, M) + 1;
+        int lPtr = 0; int rPtr = arr.length - 1;
+        while(lPtr <= rPtr)
+        {
+            int mid = (lPtr + rPtr) / 2;
+            if(arr[mid] == M) return mid + 1;
+            else
+            {
+                if(arr[mid] > M) rPtr = mid - 1;
+                else lPtr = mid + 1;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) throws IOException {
