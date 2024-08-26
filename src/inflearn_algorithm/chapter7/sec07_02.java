02a26812181598b9bcd159b116741cd10105435f
SeungWook_Lee <leesw_0924@icloud.com>
2024-08-26 14:13:00 +0900

    24.08.26

---
+++ src/inflearn_algorithm/chapter7/sec07_02.java
@@ -0,0 +1,20 @@
+package inflearn_algorithm.chapter7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sec07_02 {
+    public static void solution(int N)
+    {
+        if (N == 0) return;
+        solution(N / 2);
+        System.out.print(N % 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        solution(N);
+    }
+}
