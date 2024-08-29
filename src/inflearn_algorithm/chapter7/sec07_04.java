d5ab8481b207ef3d3e950cc57f2b3d85fb197d86
SeungWook_Lee <leesw_0924@icloud.com>
2024-08-29 22:22:06 +0900

    24.08.29 - rec

--- src/inflearn_algorithm/chapter7/sec07_04.java
+++ src/inflearn_algorithm/chapter7/sec07_04.java
@@ -5,23 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class sec07_04 {
-    static String[] memo;
-    public static void solution(int N)
+    static int[] memo;
+    public static int solution(int N)
     {
-        Long[] arr = new Long[N];
-        arr[0] = 1L;
-        arr[1] = 1L;
-        System.out.print(arr[0] + " ");
-        System.out.print(arr[1] + " ");
-        for(int i = 2; i < N; ++i)
-        {
-            arr[i] = arr[i - 2] + arr[i - 1];
-            System.out.print(arr[i] + " ");
-        }
+        if(memo[N] > 0) return memo[N];
+        if(N == 1) return memo[N] = 1;
+        else if(N == 2) return memo[N] = 1;
+        else return memo[N] = solution(N - 2) + solution(N - 1);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        solution(Integer.parseInt(br.readLine()));
+        int N = Integer.parseInt(br.readLine());
+        memo = new int[N + 1];
+        solution(N);
+        for (int i = 1; i < memo.length; ++i) System.out.print(memo[i] + " ");
     }
 }
