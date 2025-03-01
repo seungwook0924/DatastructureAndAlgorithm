7b03d3f3941983e6078d0c73eb3ba0b2c36b5c2e
SeungWook_Lee <leesw_0924@icloud.com>
2025-03-01 21:54:46 +0900

    Boj_23802

---
+++ src/Onlne_Judge/bronze/Boj_23802.java
@@ -0,0 +1,28 @@
+package Onlne_Judge.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_23802
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N * 5; ++i)
+        {
+            if(i >= N)
+            {
+                for (int j = 0; j < N; ++j) System.out.print("@");
+                System.out.println();
+            }
+            else
+            {
+                for(int j = 0; j < N * 5; ++j) System.out.print("@");
+                System.out.println();
+            }
+        }
+
+    }
+}
--- src/test.java
+++ src/test.java
@@ -9,28 +9,6 @@ public class test
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i < N; ++i) arr[i] = Integer.parseInt(st.nextToken());
-
-        int count = 0;
-        for(int i = N - 1; i > 0; --i)
-        {
-            for(int j = i - 1; j >= 0; --j)
-            {
-                if(arr[j] > arr[i])
-                {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    if(++count == K) System.out.println(arr[j] + " " + arr[i]);
-                }
-            }
-        }
-        if(count < K) System.out.println(-1);
     }
 }
