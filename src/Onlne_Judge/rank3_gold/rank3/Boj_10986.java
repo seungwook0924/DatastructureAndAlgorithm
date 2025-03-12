7e1e9eed30ab917f5ee1d27b5bb8e0437c143a6d
Lee Seungwook <leesw_0924@icloud.com>
2025-03-12 12:03:46 +0900

    Boj_10986

--- src/Onlne_Judge/rank3_gold/rank3/Boj_10986.java
+++ src/Onlne_Judge/rank3_gold/rank3/Boj_10986.java
@@ -5,30 +5,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Boj_10986 {
+public class Boj_10986
+{
     public static void main(String args[]) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // n과 m을 받아옴
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N];
-        for (int i = 0; i < N; ++i) arr[i] = Integer.parseInt(st.nextToken());
 
+        // 합 배열과 나머지를 저장하는 배열 생성
+        long[] sumArr = new long[N + 1];
+        long[] remainArr = new long[M];
         long count = 0;
 
-        long[] prefixSum = new long[N + 1];
-        long[] remainArr = new long[M];
-        for (int i = 1; i <= N; ++i)
+        // 합 배열 및 나머지 배열을 구함
+        // 1차적으로 값을 구함
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < sumArr.length; ++i)
         {
-            prefixSum[i] = arr[i - 1] + prefixSum[i - 1];
-            int remainder = (int) (prefixSum[i] % M);
-            if(remainder == 0) ++count;
+            sumArr[i] = Integer.parseInt(st.nextToken()) + sumArr[i - 1];
+            int remainder = (int) (sumArr[i] % M);
+            if(remainder == 0) ++count; // 나머지가 0인 개수를 카운트
             ++remainArr[remainder];
         }
 
-        for(int i = 0; i < M; ++i) if(remainArr[i] >= 2) count += (remainArr[i] * (remainArr[i] - 1) / 2);
+        for(int i = 0; i < M; ++i)
+        {
+            if(remainArr[i] >= 2) count += (remainArr[i] * (remainArr[i] - 1) / 2);
+        }
 
         System.out.println(count);
     }
