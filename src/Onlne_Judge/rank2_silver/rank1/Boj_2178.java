f69656f27b848857b46f0fd453e9142620c14fc9
Lee seungwook <leesw_0924@icloud.com>
2025-03-29 12:46:23 +0900

    Boj_2178 update

--- src/Onlne_Judge/rank2_silver/rank1/Boj_2178.java
+++ src/Onlne_Judge/rank2_silver/rank1/Boj_2178.java
@@ -11,8 +11,6 @@ public class Boj_2178
 {
     static int[][] arr; // 미로
     static boolean[][] visited; // 방문 체크
-    static int[] dy = { -1, 1, 0, 0 }; // y축(상, 하)
-    static int[] dx = { 0, 0, -1, 1 }; // x축(좌, 우)
     static int n;
     static int m;
 
@@ -28,42 +26,41 @@ public class Boj_2178
         arr = new int[n][m];
         for(int i = 0; i < n; ++i)
         {
-            char[] charArray = br.readLine().toCharArray();
-            for(int j = 0; j < m; ++j) arr[i][j] = Character.getNumericValue(charArray[j]);
+            char[] charArray = br.readLine().toCharArray(); // 문자열을 문자 배열로 변환
+            for(int j = 0; j < m; ++j) arr[i][j] = Character.getNumericValue(charArray[j]); // 각 문자를 숫자로 변환하여 배열에 저장
         }
 
-        BFS(0, 0);
-        for(int i = 0; i < n; ++i)
-        {
-            for(int j = 0; j < m; ++j) System.out.print(arr[i][j] + " ");
-            System.out.println();
-        }
+        BFS(0, 0); // (0, 0)부터 최단경로 탐색 시작
         System.out.println(arr[n - 1][m - 1]);
     }
 
     static void BFS(int startX, int startY)
     {
+        int[] dy = { -1, 1, 0, 0 }; // y축(상, 하)
+        int[] dx = { 0, 0, -1, 1 }; // x축(좌, 우)
         Queue<int[]> que = new LinkedList<>();
-        que.offer(new int[] {startY, startX});
-        visited[startY][startX] = true;
+        que.offer(new int[] {startY, startX}); // 큐에 시작 좌표를 넣음
+        visited[startY][startX] = true; // 시작 좌표는 방문 처리
 
-        while(!que.isEmpty())
+        while(!que.isEmpty()) // 큐가 비어있지 않았다면
         {
-            int now[] = que.poll();
-            int nowY = now[0];
-            int nowX = now[1];
+            int now[] = que.poll(); // 큐에서 좌표를 꺼냄
+            int nowY = now[0]; // y좌표
+            int nowX = now[1]; // x좌표
 
-            for(int i = 0; i < 4; ++i)
+            for(int i = 0; i < 4; ++i) // 상하좌우로 다음 탐색할 좌표를 입력
             {
-                int nextY = nowY + dy[i];
-                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i]; // 다음 탐색할 y좌표
+                int nextX = nowX + dx[i]; // 다음 탐색할 x좌표
 
-                if (nextY < 0 || nextX < 0 || nextY >= n || nextX >= m) continue; // 좌표 유효성 검사
-                if (visited[nextY][nextX] || arr[nextY][nextX] == 0) continue; // 이미 방문했거나, 벽이라면 pass
-
-                visited[nextY][nextX] = true; // 방문 표시
-                arr[nextY][nextX] = arr[nowY][nowX] + 1; // 이동한 칸 수 업데이트
-                que.offer(new int[] {nextY, nextX}); // 큐에 삽입
+                if (nextY < 0 || nextX < 0 || nextY >= n || nextX >= m) continue; // 다음 탐색할 좌표 유효성 검사
+                else if (visited[nextY][nextX] || arr[nextY][nextX] == 0) continue; // 다음 탐색할 좌표가 이미 방문했거나, 벽이라면 pass
+                else
+                {
+                    visited[nextY][nextX] = true; // 방문 표시
+                    arr[nextY][nextX] = arr[nowY][nowX] + 1; // 이동한 칸 수 업데이트(BFS 깊이)
+                    que.offer(new int[] {nextY, nextX}); // 큐에 삽입
+                }
             }
         }
     }
