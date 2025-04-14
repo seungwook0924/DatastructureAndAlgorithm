d29416a1c16c056ffc449d7c7d543f1357ad10af
Lee seungwook <leesw_0924@icloud.com>
2025-04-14 12:13:16 +0900

    Boj_18352

---
+++ src/Onlne_Judge/rank2_silver/rank2/Boj_18352.java
@@ -0,0 +1,71 @@
+package Onlne_Judge.rank2_silver.rank2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Boj_18352
+{
+    static ArrayList<Integer>[] list;
+    static int distanceArr[];
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 노드 수
+        int m = Integer.parseInt(st.nextToken()); // 엣지 수
+        int k = Integer.parseInt(st.nextToken()); // 원하는 거리 수
+        int x = Integer.parseInt(st.nextToken()); // 출발 노드
+
+        list = new ArrayList[n + 1];
+        for(int i = 1; i < n + 1; ++i) list[i] = new ArrayList<>();
+
+        distanceArr = new int[n + 1];
+        Arrays.fill(distanceArr, -1); // 거리를 -1로 초기화
+
+        for(int i = 0; i < m; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            list[from].add(to);
+        }
+
+        bfs(x);
+
+        boolean found = false;
+        for (int i = 1; i < n + 1; ++i)
+        {
+            if (distanceArr[i] == k)
+            {
+                System.out.println(i);
+                found = true;
+            }
+        }
+
+        if (!found) System.out.println(-1);
+
+    }
+
+    static void bfs(int startCity)
+    {
+        Queue<Integer> que = new LinkedList<>();
+        distanceArr[startCity] = 0; // 시작 도시는 거리가 0
+        que.add(startCity); // 큐에 시작 도시 추가
+        while (!que.isEmpty()) // 큐가 비어있지 않다면
+        {
+            int currentCity = que.poll();
+            for (int nextCity : list[currentCity]) // 인접 리스트에서 다음 도시를 가져옴
+            {
+                if (distanceArr[nextCity] == -1) // 거리가 정해지지 않았다면
+                {
+                    distanceArr[nextCity] = distanceArr[currentCity] + 1; // 다음 도시의 거리는 현재 도시의 거리 +1
+                    que.offer(nextCity);
+                }
+            }
+        }
+    }
+}
--- src/Onlne_Judge/rank3_gold/rank2/Boj_1033.java
+++ src/Onlne_Judge/rank3_gold/rank2/Boj_1033.java
@@ -8,6 +8,11 @@ import java.util.StringTokenizer;
 
 public class Boj_1033
 {
+    static ArrayList<Node>[] list;
+    static boolean[] visited;
+    static long[] ratio;
+    static int n;
+
     static class Node
     {
         int toNode;
@@ -22,20 +27,49 @@ public class Boj_1033
         }
     }
 
-    static ArrayList<Node>[] list;
-    static boolean visited[];
+    // 최대공약수
+    static long getGCD(long a, long b)
+    {
+        if(b == 0) return a;
+        else return getGCD(b, a % b);
+    }
+
+    // 최소공배수
+    static long getLCM(long a, long b)
+    {
+        return a * b / getGCD(a, b);
+    }
+
+    static void dfs(int i)
+    {
+        visited[i] = true;
+
+        for (Node node : list[i])
+        {
+            if (!visited[node.toNode])
+            {
+                ratio[node.toNode] = ratio[i] * node.q / node.p;
+                dfs(node.toNode);
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
         list = new ArrayList[n];
-        for(int i = 0; i < n; ++i) list[i] = new ArrayList<>();
+        for (int i = 0; i < n; ++i) list[i] = new ArrayList<>();
+        ratio = new long[n];
+        visited = new boolean[n];
 
-        for(int i = 0; i < n - 1; ++i)
+        long lcm = 1;
+
+        for (int i = 0; i < n - 1; ++i)
         {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -44,7 +78,19 @@ public class Boj_1033
             int q = Integer.parseInt(st.nextToken());
 
             list[a].add(new Node(b, p, q));
+            list[b].add(new Node(a, q, p)); // 양방향 추가
+
+            long pqLCM = getLCM(p, q); // p와 q의 최소공배수를 구함
+            lcm = getLCM(lcm, pqLCM); // 이전 최소 공배수와 현재 최소공배수의 최소공배수를 구함
         }
 
+        ratio[0] = lcm;
+        dfs(0);
+
+        long gcd = ratio[0];
+        for (int i = 1; i < n; ++i) gcd = getGCD(gcd, ratio[i]);
+        for (int i = 0; i < n; ++i) sb.append(ratio[i] / gcd).append(" ");
+
+        System.out.println(sb);
     }
 }
